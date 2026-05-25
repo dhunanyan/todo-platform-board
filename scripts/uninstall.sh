90217e5261b4b912d9330b0b7bfa39467797261e
dhunanyan <davit.hunanyan@me.com>
2026-05-25 20:35:10 +0200

    chore: add uninstall.sh and remove .env

---
+++ scripts/uninstall.sh
@@ -0,0 +1,6 @@
+rm -rf "/Applications/Task Flow Board.app"
+rm -rf "$HOME/Library/Application Support/task-flow-board"
+rm -rf "$HOME/Library/Application Support/Task Flow Board"
+rm -rf "$HOME/Library/Caches/com.taskflow.board"
+rm -rf "$HOME/Library/Logs/task-flow-board"
+rm -rf "$HOME/Library/Preferences/com.taskflow.board.plist"
